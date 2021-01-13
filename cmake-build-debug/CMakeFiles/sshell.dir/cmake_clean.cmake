4854a57eaa8377a45942fabd2c10705eee3794fd
ardenlee65 <50338472+ardenlee65@users.noreply.github.com>
2021-01-12 22:50:43 -0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/sshell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sshell.dir/main.c.o"
+  "libsshell.dll.a"
+  "sshell.exe"
+  "sshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
